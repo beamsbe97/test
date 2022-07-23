122710042c8c1ddd7a6b8eebe5050e22dc09888e
beamsbe97 <nguyenquocan01@gmail.com>
2022-07-23 19:52:06 +0800

    commit message

---
+++ Lab3.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Lab3
+{
+    static Scanner input = new Scanner(System.in);
+    public static void main (String [ ] args)
+    {
+
+    }
+    public static void PetrolPurchase (String [ ] args)
+    {
+        String station;      
+        String type;
+        double quantity;
+        double price;
+        int discount;
+        
+
+        System.out.print("Enter the station:");
+        station = nextLine();
+        System.out.print("Enter quantity in liter:");
+        quantity = nextLine();
+        System.out.print("Enter type of petrol:");
+        type = nextLine();
+
+    }
+}  
