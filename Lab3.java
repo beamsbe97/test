d6b64b486c89ddca5f372249d9bed4a7f9a25a2a
beamsbe97 <nguyenquocan01@gmail.com>
2022-07-23 21:36:04 +0800

    commit message

--- Lab3.java
+++ Lab3.java
@@ -7,21 +7,109 @@ class Lab3
     {
 
     }
-    public static void PetrolPurchase (String [ ] args)
-    {
-        String station;      
-        String type;
-        double quantity;
-        double price;
-        int discount;
-        
 
-        System.out.print("Enter the station:");
-        station = nextLine();
-        System.out.print("Enter quantity in liter:");
-        quantity = nextLine();
-        System.out.print("Enter type of petrol:");
-        type = nextLine();
-
-    }
 }  
+
+class PetrolPurchase
+{
+    private String station;      
+    private String type;
+    private double quantity;
+    private double price;
+    private int discount;
+        
+    public static void PetrolPurchase (String s, double q, String t, double p, int d)
+    {
+        this.station = s;
+        this.quantity = q;
+        this.type = t;
+        this.price = p;
+        this.discount = d;
+    }
+
+    //Accessor methods
+    public String getStation()
+    {
+        return station;
+    }
+
+    public String getType()
+    {
+        return type;
+    }
+
+    public double getQuantity()
+    {
+        return quantity;
+    }
+
+    public double getPrice()
+    {
+        return price;
+    }
+
+    public int getDiscount()
+    {
+        return discount;
+    }
+
+    //Mutator methods
+    public void setStation(String station)
+    {
+        this.station = station;
+    }
+
+    public void setType(String type)
+    {
+        this.type = type;
+    }
+
+    public void setPrice(double price)
+    {
+        this.price = price;
+    }
+
+    public void setQuantity(double quantity)
+    {
+        this.quantity = quantity;
+    }
+
+    public void setDiscount(int discount)
+    {
+        this.discount = discount;
+    }
+
+    private setInfo(String s, double q, String t, double p, int d)
+    {
+        this.station = s;
+        this.quantity = q;
+        this.type = t;
+        this.price = p;
+        this.discount = d;
+    }
+
+    private double computePayment()
+    {
+        double payment = price * quantity;
+    }
+    
+    public double getPayment()
+    {
+        return payment;
+    }
+
+    public void printInfo()
+    {
+        System.out.printf("")
+    }
+} 
+
+
+/* System.out.print("Enter the station:");
+station = nextLine();
+System.out.print("Enter quantity in liter:");
+quantity = nextLine();
+System.out.print("Enter type of petrol:");
+type = nextLine();
+System.out.print("Enter discount:");
+discount = nextLine(); */ 
