898918186a44a1b54ae86ba5be52155b136f9da0
beamsbe97 <nguyenquocan01@gmail.com>
2022-08-07 23:01:52 +0800

    Lab4 version 2

--- AnNguyen_Lab4.java
+++ AnNguyen_Lab4.java
@@ -1,70 +1,96 @@
 import java.util.Scanner;
 
-enum Type {Equilateral, Isosceles, Scalene, NotTriangle}
+enum Type {
+    Equilateral, Isosceles, Scalene, NotTriangle
+}
 
-class AnNguyen_Lab4
-{
-    public static void main(String[] args)
-    {
-        int a;
-        int b;
-        int c;
+class AnNguyen_Lab4 {
+    public static void main(String[] args) {
+        PossibleTriangle pt1 = new PossibleTriangle(4, 5, 6);
+        pt1.displayInfo();
+
+        
+        PossibleTriangle pt2 = new PossibleTriangle(1,2,3);
+        pt2.displayInfo();
+         
+        PossibleTriangle pt3 = new PossibleTriangle(4,5,5);
+        pt3.displayInfo();
+         
+        PossibleTriangle pt4 = new PossibleTriangle(6,6,6);
+        pt4.displayInfo();
         
     }
 }
 
-class PossibleTriangle
-{
+class PossibleTriangle {
     private int a;
     private int b;
     private int c;
     Type pt;
 
-    //Default constructor
-    public PossibleTriangle(){
-        
+    // Default constructor
+    public PossibleTriangle() {
+
     }
 
-    //Other constructor
-    public PossibleTriangle(int a, int b, int c)
-    {
+    // Other constructor
+    public PossibleTriangle(int a, int b, int c) {
         this.a = a;
         this.b = b;
         this.c = c;
     }
 
-    private boolean isTriangle()
-    {
-        if (a == 0 || b==0 || c==0)
+    private boolean isTriangle() {
+        if (a + b > c || c + b > a || a + c > b)
             return true;
         else
             return false;
     }
-    
-    private Type getType()
-    {
-        while (a > 0 && b > 0 && c > 0)
-        {
-            if (a == b || b==c || a==c)
+
+    private Type getType() {
+        if (a > 0 && b > 0 && c > 0) {
+            if (a == b || b == c || a == c)
                 pt = Type.Isosceles;
-            else if (a == b && b == c && a==c)
-                pt = Type.Equilateral;    
+            else if (a == b && b == c && a == c)
+                pt = Type.Equilateral;
+            else if (a != b && b != c && a != c)
+                pt = Type.Scalene;
+            else
+                pt = Type.NotTriangle;
         }
+
+        
 
         return pt;
     }
-    
-    public void displayInfo()
+
+    public void displayInfo() 
     {
-        if (isTriangle())
-            switch(pt)
-            {
-                case Isosceles: System.out.println("Isosceles triangle");
-                case Equilateral: System.out.println("Equilateral triangle");
-                case Scalene: System.out.println("Scalene");
+        System.out.printf("\na = %d, b = %d, c = %d%n", a,b,c);
+        System.out.printf("%s%n", getType());
+        /*if (isTriangle() == true) 
+        {
+            //System.out.printf("Type: %s", getType());
+
+            switch (getType()) {
+                case Isosceles:
+                    System.out.println("Isosceles triangle");
+                case Equilateral:
+                    System.out.println("Equilateral triangle");
+                case Scalene:
+                    System.out.println("Scalene triangle");
+                default:
+                    System.out.println("A triangle");
+                    break;
             }
-        
+        } 
+
         else
+        {
             System.out.println("Not a triangle");
+        }*/
+
+        System.out.printf("-----------------------------------------");
+            
     }
 }
