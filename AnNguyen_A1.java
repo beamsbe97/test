caa6657efcfbb886577237e8e541a59bc7479a99
beamsbe97 <37875772+beamsbe97@users.noreply.github.com>
2022-10-12 21:02:46 +0800

    Add files via upload
    Assignment 1 CSIT121

---
+++ AnNguyen_A1.java
@@ -0,0 +1,129 @@
+import java.util.Scanner;
+import java.ArrayList;
+
+enum ZodiacInfo{Aries, Taurus, Gemini, Cancer, Leo, Vigro, Libra, Scorpio, Sagittarius, Capricorn, Aquarius, Pisces}
+enum ZodiacType{ARI, TAU, GEM, CAN, LEO, VIR, LIB, SCO, SAG, CAP, AQU, PIS}
+
+class AnNguyen_A1{
+    private static Scanner input = new Scanner(System.in);
+
+    //NEED TO USE DO WHILE !!!!!!!!!!!!!!!!!!!!!
+
+    private static void displayZodiacTypeInfo(){
+
+    }
+
+    private static ZodiacType getAnElement(){
+
+    }
+
+    private Set getASet(){
+
+    }
+
+    private static void displayMenu(){
+
+    }
+
+    private static void unionExample(){
+
+    }
+
+    private static void interesctionExample(){
+
+    }
+
+    private static void subsetExample(){
+
+    }
+
+    private static void differenceExample(){
+
+    }
+
+    private static void complementExample(){
+
+    }
+
+    private static void equalityExample(){
+
+    }
+
+    private static void distributiveLaw_1(){
+
+    }
+
+    private static void distributiveLaw_2(){
+
+    }
+
+    public static void displaySubmenu(){
+
+    }
+
+    public static void anExample(){
+
+    }
+
+    public static void main(String[] args){
+        
+    }
+}
+
+class Set{
+
+
+    public Sest(){}
+
+    public Set(Set otherSet){
+
+    }
+
+    public boolean isEmpty(){
+
+    }
+
+    public int cardinality(){
+
+    }
+
+    public boolean belongTo(ZodiacType element){
+
+    }
+
+    public void addElement(ZodiacType element){
+
+    }
+
+    public boolean subset(Set otherSet){
+
+    }
+
+    public void union(Set otherSet){
+
+    }
+
+    public void intersection(Set otherSet){
+
+    }
+
+    public void difference(Set otherSet){
+
+    }
+
+    public Set complement(){
+
+    }
+
+    public boolean equality(Set otheSet){
+
+    }
+
+    public String toString(){
+        String.format(null, null);
+    }
+
+    public String getZodiaInfoFormat(){
+
+    }
+}
